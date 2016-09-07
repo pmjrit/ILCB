82996e86d862588a516063dd41ca7932cd21f56b
Chris Kitch <chris@coderage.co.uk>
2016-09-07 09:10:54 +0100

    FAQ update, new image and style changes

--- js/faq.js
+++ js/faq.js
@@ -2,67 +2,56 @@ var faq = [{
     "id": "what-about-socialism",
     "question": "... isn't he a socialist?",
     "answer": 
-        "Jeremy Corbyn is a <a href='http://www.politifact.com/truth-o-meter/article/2015/aug/26/bernie-sanders-socialist-or-democratic-socialist/'><i>democratic socialist</i></a>. He believes that our current economic system isn't doing enough for poor and middle-class Americans and that democratic change is needed to create a more fair and just America.<br><br>" + 
-        "But this isn't radical or scary! Many of the programs instituted by Franklin D. Roosevelt and Lyndon Johnson that we take for granted today &mdash; such as Medicare, the 40-hour work week, the minimum wage, and Social Security &mdash; <a href='http://www.theatlantic.com/politics/archive/2015/11/bernie-sanders-makes-his-pitch-for-socialism/416913/'>can be considered socialist programs</a>."
+        "Jeremy Corbyn is a <a href='http://www.politifact.com/truth-o-meter/article/2015/aug/26/bernie-sanders-socialist-or-democratic-socialist/'><i>democratic socialist</i></a>. He believes that our current economic system isn't doing enough for the poor and the middle-class in our society and that democratic change is needed to create a fairer and more just society.<br><br>" + 
+        "But this isn't radical or scary. Many of the programs instituted by post war governments that we take for granted today &mdash; such as the NHS, the limited hours of work, the minimum wage, and Social Security &mdash; could be described as socialist programs."
 },{
-    "id": "isnt-he-raising-taxes",
-    "question": "... I heard he wants to raise taxes!",
-    "answer": 
-        "If you're one of the 1.5% of Americans making more than $231,450 a year, your <i>marginal</i> tax rate will go up slightly &mdash; money you earn above $231,450 will be taxed at 37% instead of 33%.<br><br>" +
-        "If you're one of the 0.6% of Americans making more than $500,000 a year, your marginal rate will go up from 39.6% to 43%. Above $2,000,000 the rate will be 48%, and above $10,000,000 the rate will be 52%. These are tax increases that will only affect the very, <i>very</i> wealthy.<br><br>" +
-        "On the other hand, the vast majority of Americans <a href='http://datatitian.com/bernie-tax/'>will see significant savings</a> when factoring in tax and healthcare changes under Bernie's plans."
+    "id": "wouldn't-he-increase-taxes",
+    "question": "... I heard he wants to increase taxes!",
+   "answer": "Most people will be better off under Corbyn's tax plan. " +
+		"These are the proposals: " + 
+		"<ul><li>Reverse the cuts to Corporation Tax.</li>" +
+		"<li>Reverse cuts to Inheritance Tax</li>" +		
+		"<li>Introduce a <a href=\"https://en.wikipedia.org/wiki/Land_value_tax\" target=\"_blank\">land value tax</a></li>" +
+		"<li>Increase the top rate of tax to 50p from the current rate 45p.  This rate is only paid on income over the £150,000 threshold.</li></ul>" +
+		"<p>Jeremy has said he would intervene to enforce laws on British overseas territories and dependencies if they do not comply with UK tax law, introduce anti-avoidance measures into UK law, and invest in HMRC so they can hire the staff needed to collect the tax the country badly needs.</p>"
 },{
     "id": "is-he-electable",
     "question": "... is he even electable?",
     "answer": 
-        "Absolutely. Bernie probably has a better chance in the general election than Hillary would have:<br><br>" +
-        "Bernie performs better than Hillary does in all hypothetical match-ups against Republicans in <a href='http://www.realclearpolitics.com/epolls/2016/president/2016_presidential_race.html'>poll after poll</a>, and on average does 7.4% better against Trump (Sanders +11.8% against Trump compared to Clinton +6.2% against Trump).<br><br>" +
-        "Bernie significantly outperforms Hillary in <a href='http://ivn.us/2015/09/15/ivn-presidential-poll-bernie-sanders-donald-trump-ben-carson-lead/'>surveys of <i>independent voters</i></a>, and with 30-40% of Americans identifying as independents, they will play the deciding role in the general election.<br><br>" +
-        "And Bernie has a big lead in favorability, with a <a href='http://elections.huffingtonpost.com/pollster/bernie-sanders-favorable-rating'><b>+11%</b> net favorability</a> rating among all Americans, compared to <a href='http://elections.huffingtonpost.com/pollster/hillary-clinton-favorable-rating'>Hillary's <b>-13%</b> net favorability rating</a>. <b>No</b> presidential candidate has <a href='https://lh6.googleusercontent.com/-bBLIXEsM56U/VoRD8zsEUlI/AAAAAAABxjs/CAnZZGXpO7M/w874-h894-no/OIusBY8.jpg'><b>ever</b> won with a negative favorability rating</a>."
+        "Corbyn has attracted a massive numbers of new and very enthusiastic members.  With the right strategy, and hard work by us all, of course he is electable. That means persuading people of the case  for Corbyn as a different kind of leader and for the kind of society he wants to help build. He and his team are appealing to people who <span class=\"jcblack\"><strong>used to be cynical</strong></span> about politics or resigned to accepting whatever is dished out to them.  Their involvement and support will help too."
 },{
+/* Not sure this should be included at this stage.  It becomes more relevant after the leadership election */
     "id": "can-he-get-stuff-done",
-    "question": "... how could he get anything done with a Republican Congress?",
+    "question": "... how could he get anything done with so many of PLP being against him?",
     "answer": 
-        "Bernie is actually well-known for his ability to compromise to get things done without sacrificing his values. In the House, he was known as the Amendment King, and passed more amendments, <a href='http://www.dailykos.com/story/2015/10/6/1428616/-Bernie-Sanders-What-the-Hell-Have-You-Done-for-Us-Lately'>addressing exclusively progressive goals</a>, <a href='http://www.alternet.org/election-2016/bernie-gets-it-done-sanders-record-pushing-through-major-reforms-will-surprise-you'>than any other legislator</a>, by forging cross-party coalitions.<br><br>" + 
-        "He has <a href='http://www.nationaljournal.com/s/71225/bernie-sanders-is-loud-stubborn-socialist-republicans-like-him-anyway'>earned respect from Republicans</a> ranging from <a href='http://www.washingtonexaminer.com/mccain-calls-sanders-x-rated-but-an-honest-man/article/2577874'>John McCain</a> to the ultra-conservative <a href='http://www.bloomberg.com/features/2016-bernie-sanders-fundraising/'>Jim Inhofe</a>. If any Democratic president can reach across the aisle to work with a stubborn Republican Congress, it's Jeremy Corbyn."
+        "If was elected as Prime Minister it is very likely there would be a realignment within the PLP. Having won an election his standing and authority would increase."
 },{
-    "id": "where-is-he-on-guns",
-    "question": "... isn't he too weak on gun control?",
-    "answer": 
-        "Hardly. He has <a href='http://votesmart.org/candidate/evaluations/27110/bernie-sanders/37'>consistent high ratings</a> from the Brady Campaign to Prevent Gun Violence and a <a href='http://www.politifact.com/truth-o-meter/statements/2016/jan/20/bernie-s/bernie-sanders-nra-report-card-d-minus-most-recent/'>D- rating</a> from the NRA.<br><br>" +
-        "His position is that <a href='http://thehill.com/blogs/blog-briefing-room/255741-sanders-sees-consensus-for-serious-gun-control'>a broad consensus of common-sense gun control measures</a> is possible if neither side is alienated through overly divisive rhetoric."
-},{
-    "id": "would-he-kill-obamacare",
-    "question": "... I heard he wants to get rid of Obamacare!",
+    "id": "position-on-nhs",
+    "question": "...what about the NHS?",
     "answer":
-        "Bernie wants to <i>expand</i> health care coverage, not get rid of it!<br><br>" +
-        "Obamacare has made things a lot better, but it's only a step in the right direction: Americans are still <a href='http://www.theatlantic.com/health/archive/2014/06/us-healthcare-most-expensive-and-worst-performing/372828/'>paying more for healthcare than any other country</a>, and <b>more than 10%</b> of us still don't have health insurance.<br><br>" +
-        "Bernie's <a href='https://berniesanders.com/medicareforall/'>Medicare-for-all</a> proposal will do just what it says &mdash; provide coverage for every American citizen, while saving the average American family $2000&mdash;$4000 per year. <a href='http://berniecare.org/'>[Try the calculator!]</a>"
+        "A Corbyn led government would:<ul><li>End health service privatisation and bring services into a secure, publicly-provided NHS.</li><li>Integrate the NHS and social care for older and disabled people.</li><li>Ensure parity for mental health services.</li></ul>"
 },{
     "id": "what-about-foreign-policy",
     "question": "... does he have any experience with foreign policy?",
     "answer": 
-        "While Bernie's main focus is on domestic issues, he has shown remarkable foresight when it comes to foreign policy decisions. He was a member of a small minority voting against the War in Iraq in 2003, <a href='https://www.youtube.com/watch?v=NdFw1btbkLM'>arguing in an impassioned speech</a> that overthrowing Saddam Hussein would result in &quot;disaster&quot;. He was right.<br><br>" +
-        "And Bernie is one of very few U.S. politicians to <a href='http://takingnote.blogs.nytimes.com/2016/03/22/straight-talk-from-bernie-sanders-on-the-israeli-palestinian-conflict/'>criticize the disproportionate nature of Israel's war in Gaza</a>, to <a href='http://www.commondreams.org/news/2016/04/17/bernie-sanders-us-cant-be-blackmailed-saudi-arabia'>bring attention to the U.S.'s questionable ties to Saudi Arabia</a>, and to <a href='https://topherhallquist.wordpress.com/2016/03/11/full-transcript-of-the-1985-sanders-interview-sampled-in-univisions-debate-with-some-commentary/'>point out the negative consequences of the U.S.'s Latin American interventions of the 1980s</a>.<br><br>" +
-        "In fact, even Lawrence Korb, <i>Reagan</i>'s Assistant Defense Secretary, has praised Bernie's foreign policy philosophy <a href='http://www.politico.com/magazine/story/2016/02/bernie-sanders-foreign-poicy-213619'>as one of pragmatic realism</a>, comparing its emphasis on restraint and diplomacy to the foreign policy of Eisenhower."
+        "Corbyn has been deeply involved in the debate on foreign policy throughout his career.  His approach emphasises peace, human rights, international law, and democracy."
 },{
     "id": "how-will-he-fund-proposals",
     "question": "... how will he pay for all this?",
-    "answer": "Every dollar in Jeremy Corbyn's proposals is matched by a corresponding dollar raised in revenue &mdash; <a href='https://berniesanders.com/issues/how-bernie-pays-for-his-proposals/'>it's all accounted for</a>.<br><br>For example, the $75 billion/year College For All plan will be paid for by a <a href='http://www.peri.umass.edu/fileadmin/pdf/ftt/Pollin--Heintz--Memo_on_FTT_Rates_and_Revenue_Potential_w_references----6-9-12.pdf'>tax on Wall Street speculation</a>, while the $100 billion/year Rebuild America Act will be paid for by <a href='http://www.fas.org/sgp/crs/misc/R40623.pdf'>taxing corporate offshore income</a>."
+    "answer": "Economic growth in high skill, high tech and low carbon enterprises stimulated and led by the National and Regional Investment Banks combined with changes to the tax regime (see above)."
 },{
-    "id": "can-he-get-minority-support",
-    "question": "... aren't his supporters all young white men?",
+    "id": "supporters",
+    "question": "... I worry about his supporters",
     "answer": 
-        "This is a false narrative that keeps getting pushed. In reality, Bernie has the support of <a href='http://www.usatoday.com/story/news/politics/elections/2016/01/14/bernie-sanders-hillary-clinton-women-millennials/78810110/'>50% of young women</a>, is viewed more favorably than Hillary by <a href='https://twitter.com/ZaidJilani/status/692818532212543488'>Hispanics</a> and <a href='http://www.gallup.com/opinion/polling-matters/187577/bernie-sanders-popular-candidate-among-asian-americans.aspx'>Asian-Americans</a> nationwide, and does better among <a href='http://www.dailykos.com/story/2015/10/13/1431846/-Bernie-Sanders-is-doing-better-among-African-Americans-than-among-whites-in-Pennsylvania'>African-Americans than whites</a> in Pennsylvania and some other states.<br><br>" +
-        "And this is not surprising given Bernie's unwavering commitment to rights for all Americans throughout his life. He took part in <a href='https://www.youtube.com/watch?v=TPWcmPE5shc'>MLK's March on Washington in 1963</a>, <a href='http://www.vox.com/2015/9/9/9295867/bernie-sanders-gay-soldiers'>stood up for the rights of LGBT soldiers in 1995</a>, and has maintained a <a href='http://votesmart.org/candidate/evaluations/27110/bernie-sanders/75'>100% voting record with Planned Parenthood</a> for his entire career."
+        "Corbyn's supporters are from a wide variety of age groups and walks of life. After attending a meeting in Derby Paul Rogers (Bradford University) <a href=\"https://www.opendemocracy.net/paul-rogers/corbyn-crowd-and-its-signal\" target=\"_blank\">reported</a> <i>\"I doubt that any other politician could have attracted such a crowd at the present time, nor for many years, at least in the English midlands. The mood was positive, appreciative but not 'over the top'. <strong>It was as far from a cult as you could imagine</strong>\". [emphasis added]. Why not <a href=\"http://www.jeremyforlabour.com/rallies\" target=\"_blank\">go to a rally?</a></i>"
 },{
     "id": "how-old-is-he-again",
     "question": "... he's too old!",
-    "answer": "At 74, Jeremy Corbyn would be the oldest president ever elected to a first term. But so would Donald Trump. And Hillary Clinton would tie for oldest. As medical science improves, people are able to be fit enough, mentally and physically, to run a country well into their 70s in a way that wasn't possible before. And <a href='http://www.cnn.com/2016/01/28/politics/bernie-sanders-medical-records/'>Bernie is in very good health</a>."
+    "answer": "At 71, Jeremy Corbyn would be the oldest Prime Minister ever elected to a first term. He is now a fit and healthy 67 year old. Angela Eagle said of him during the EU Referendum that he was pursuing an agenda that would make a 25-year-old tired. He has demonstrated remarkable stamina during the current leadership campaign."
 }, {
     "id": "I-am-still-unsure",
     "question": "Hmm, these are some good points, but I'm still unconvinced. Where can I learn more?",
     "answer": 
-        "To learn more about Jeremy Corbyn's positions on more than 50 different issues, check out <a href='http://feelthebern.org'>FeelTheBern.org</a>.<br><br>" +
-        "You can also watch Robert Reich challenge these critiques of Bernie much better than I can <a href='https://www.youtube.com/watch?v=VfIhonVoFSg'>in his recent video</a>."
+        "To learn more about Jeremy Corbyn's policies, please visit <a href='http://www.jeremyforlabour.com/pledges'>www.jeremyforlabour.com/pledges</a>.<br /><br />" +
+        "[speech in support - video]"
 }];
